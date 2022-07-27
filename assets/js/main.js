9e8482a8ca0c9c2f4db16fc98ab728dfd3dc42dd
Sherydan <tobar.luis.c@gmail.com>
2022-07-27 01:18:03 -0400

    added basic html markup, added some coding elements

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+let txtTodoInput = document.querySelector('#txtTodoInput')
+let btnAddTodo = document.querySelector('#btnAddTodo');
+let todoListItems = document.querySelector('#todoListItems')
+
+
+let taskListBody = document.querySelector('#taskListBody')
+let todoArray = [];
+
+let todoItemMaker = (todoText, id) =>{
+    return `
+    <li class="list-group-item d-flex justify-content-between align-items-start">
+        <input class="form-check-input me-1" type="checkbox" data-task-id="${id}">
+        <label class="form-check-label" for="firstCheckbox" >${todoText}</label>
+        <i class="fa-solid fa-trash-can"></i>
+    </li>
+    `
+}
+
+let addTodoItem = () => {
+    if (txtTodoInput.value === "" || txtTodoInput.value === " ") {
+        return 
+    }
+    todoArray.push({
+        id: 1,
+        text: txtTodoInput.value,
+        completed: false
+
+    })
+
+    console.log(todoArray)
+    todoListItems.innerHTML +=  todoItemMaker(txtTodoInput.value, 1);
+    txtTodoInput.value = "";
+}
+
+let removeTodoItem = () => {
+    
+}
+
+let completeTodoTask = () => {
+    
+}
+
+btnAddTodo.addEventListener('click', () => {
+    addTodoItem();
+})
+
+taskListBody.addEventListener('click', (e) =>{
+    if (e.target.type = "checkbox") {
+        
+    }
+    
+})
+
+
+
+
+
