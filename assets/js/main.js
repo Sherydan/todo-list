9f2074c9169c7f453ab0ddb24c71e3cf04d13fb8
Sherydan <tobar.luis.c@gmail.com>
2022-07-28 01:35:41 -0400

    added complete task and remove item functionality to array

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,30 +17,56 @@ let todoItemMaker = (todoText, id) =>{
 }
 
 let addTodoItem = () => {
+    // add new item to list
     if (txtTodoInput.value === "" || txtTodoInput.value === " ") {
         return 
     }
 
-    let uuid = todoArray.length +1 
+    // let uuid = todoArray.length +1 
+    // todoArray.push({
+    //     id: uuid,
+    //     text: txtTodoInput.value,
+    //     completed: false
+    // });
+
+    // add id, text and completed status to todoArray
+    let uuid = todoArray.length + 1;
     todoArray.push({
         id: uuid,
         text: txtTodoInput.value,
         completed: false
     });
 
-    
+    console.log(todoArray)
 
     todoListItems.innerHTML +=  todoItemMaker(txtTodoInput.value, uuid);
     txtTodoInput.value = "";
 }
 
 let removeTodoItem = (removeElement) => {
-    console.log(removeElement)
+    // remove item from todoArray where id object matches removeElement.dataset.taskid
+    todoArray.forEach(todo => {
+        if (todo.id === Number(removeElement.dataset.taskid)) {
+            todoArray.splice(todoArray.indexOf(todo), 1);
+        }
+    }
+    )
     removeElement.parentElement.remove();
-    
 }
 
 let completeTodoTask = (inputElement) => {
+    console.log(inputElement.dataset.taskid)
+    // change completed status to true on todoArray where id matches inputElement.dataset.taskid or change to false if already true
+    todoArray.forEach(todo => {
+        if (todo.id === parseInt(inputElement.dataset.taskid)) {
+            todo.completed = !todo.completed;
+        }
+    }
+    )
+
+    console.log(todoArray)
+
+
     
     if (!inputElement.checked) {
         inputElement.parentElement.classList.remove('text-decoration-line-through')
